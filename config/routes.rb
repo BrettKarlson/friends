0517b70b5013f7b0096463c4b5fec46189a6c6a0
Brett Karlson <brettjkarlson@gmail.com>
2021-08-22 18:11:44 +0100

    user id associated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
-Rails.application.routes.draw do
-  devise_for :users
-  resources :friends
+Rails.application.routes.draw do   
   root 'home#index'
   get 'home/about'
+  resources :friends
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
